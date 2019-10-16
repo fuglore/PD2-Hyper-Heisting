3ecd57b4aaaa6e8717579890d55225ab4be163b8
Fuglore <44823551+fuglore@users.noreply.github.com>
2019-10-16 14:19:00 -0300

    Fixed strings not working

--- lua/localizationmanager.lua
+++ lua/localizationmanager.lua
@@ -24,14 +24,14 @@ Hooks:Add("LocalizationManagerPostInit", "shin_assaultcornertext", function(loc)
 		["hud_assault_FG_cover4"] = "FIGHT ON",
 		["hud_assault_FG_cover5"] = "THE WHEEL OF FATE IS TURNING",
 		["hud_assault_FG_cover6"] = "THIS IS TUNA WITH BACON",
-		["hud_assault_FG_cover7"] = "THIS IS TRUE LOVE WE MAKIN"
-		["menu_toggle_one_down"] = "SHIN SHOOTOUT"
-		["menu_one_down"] = "SHIN SHOOTOUT"
+		["hud_assault_FG_cover7"] = "THIS IS TRUE LOVE WE MAKIN",
+		["menu_toggle_one_down"] = "SHIN SHOOTOUT",
+		["menu_one_down"] = "SHIN SHOOTOUT",
 		["menu_cs_modifier_heavies"] = "All enemies except Bulldozers and FBIs now have body armor.",
 		["menu_cs_modifier_heavy_sniper"] = "Adds a chance for Sniperdozers to spawn.",
 		["menu_cs_modifier_dozer_lmg"] = "EVERYTHING IS HORRIBLE!",
-		["menu_cs_modifier_dozer_immune"] = "Enemies are much more aggressive."
-		["menu_cs_modifier_shield_phalanx"] = "Shotgunners have a chance to be replaced by a Gensec Saiga SWAT."
+		["menu_cs_modifier_dozer_immune"] = "Enemies are much more aggressive.",
+		["menu_cs_modifier_shield_phalanx"] = "Shotgunners have a chance to be replaced by a Gensec Saiga SWAT.",
 		["menu_cs_modifier_no_hurt"] = "SHIN SHOOTOUT is now enabled."
 	})
 end)
