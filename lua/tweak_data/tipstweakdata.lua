7b27d1d120f563a9fe8a7c78ee89db2806937585
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-01-08 16:19:28 -0300

    hint updates

--- lua/tweak_data/tipstweakdata.lua
+++ lua/tweak_data/tipstweakdata.lua
@@ -10,7 +10,7 @@ Hooks:PostHook(TipsTweakData, "init", "hh_tips", function(self)
     table.insert(self.tips, 
 		{
 			cat_index = 2,
-			image = "ninjas",
+			image = "hh_ninjas",
 			consoles = true,
 			category = "hh"
 		}
@@ -50,6 +50,38 @@ Hooks:PostHook(TipsTweakData, "init", "hh_tips", function(self)
 	table.insert(self.tips, 
 		{
 			cat_index = 7,
+			image = "crimenet_difficulty",
+			consoles = true,
+			category = "hh"
+		}
+	)
+	table.insert(self.tips, 
+		{
+			cat_index = 8,
+			image = "enemy_flashbang",
+			consoles = true,
+			category = "hh"
+		}
+	)
+	table.insert(self.tips, 
+		{
+			cat_index = 9,
+			image = "hh_shotgunners",
+			consoles = true,
+			category = "hh"
+		}
+	)
+	table.insert(self.tips, 
+		{
+			cat_index = 10,
+			image = "hh_shotgunners",
+			consoles = true,
+			category = "hh"
+		}
+	)
+	table.insert(self.tips, --remember fuglore, this is the hyper heisting discord tip, remember that
+		{
+			cat_index = 11,
 			image = "crimenet_fbifiles",
 			consoles = true,
 			category = "hh"
@@ -62,3 +94,42 @@ Hooks:PostHook(TipsTweakData, "init", "hh_tips", function(self)
 		end
 	end
 end)
+
+function TipsTweakData:get_a_tip()
+	local tip = self.tips[math.random(#self.tips)]
+	
+	if tip and tip.category and tip.category == "trivia" and math.random() < 0.6 then
+		tip = self.tips[math.random(#self.tips)]
+		--log("itrerolledmaybe")
+		if tip.category and tip.category == "trivia" and math.random() < 0.4 then
+			tip = self.tips[math.random(#self.tips)]
+			--log("itrerolledmaybe")
+			if tip.category and tip.category == "trivia" and math.random() < 0.2 then
+				tip = self.tips[math.random(#self.tips)]
+				--log("thisisthelastreroll")
+				if tip.category and tip.category == "trivia" then
+					--log("how")
+				end
+			end		
+		end
+	end
+	
+	local image_exists = DB:has(Idstring("texture"), "guis/textures/loading/hints/" .. tip.image)
+	
+	if not image_exists then
+		Application:error("Warning: missing loading hint image: " .. tip.image)
+
+		return nil
+	end
+
+	local title_id = "loading_" .. tip.category .. "_title"
+	local text_id = "loading_" .. tip.category .. "_" .. tip.cat_index
+
+	return {
+		image = tip.image,
+		index = tip.cat_index,
+		total = self.category_totals[tip.category],
+		title = managers.localization:to_upper_text(title_id),
+		text = managers.localization:text(text_id)
+	}
+end
