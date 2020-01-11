059c3dcd636a02ffd284f2e1583b00c28032ea00
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-01-11 00:38:48 -0300

    fudge.

--- lua/aitweaks/groupaistatebase.lua
+++ lua/aitweaks/groupaistatebase.lua
@@ -572,46 +572,6 @@ function GroupAIStateBase:_try_use_task_spawn_event(t, target_area, task_type, t
 	end
 end
 
-function GroupAIStateBase:_radio_chatter_clbk()
-	if self._ai_enabled then
-		local optimal_dist = 800
-		local best_dist, best_cop, radio_msg = nil
-
-		for _, c_record in pairs(self._player_criminals) do
-			for i, e_key in ipairs(c_record.important_enemies) do
-				local cop = self._police[e_key]
-				local use_radio = cop.char_tweak.use_radio
-
-				if use_radio then
-					if cop.char_tweak.radio_prefix then
-						use_radio = cop.char_tweak.radio_prefix .. use_radio
-					end
-
-					local dist = math.abs(mvector3.distance(cop.m_pos, c_record.m_pos))
-
-					if not best_dist or dist < best_dist then
-						best_dist = dist
-						best_cop = cop
-						radio_msg = use_radio
-					end
-				end
-			end
-		end
-
-		if best_cop then
-			best_cop.unit:sound():play(radio_msg, nil, true)
-		end
-	end
-
-	self._radio_clbk = callback(self, self, "_radio_chatter_clbk")
-	if managers.groupai:state():whisper_mode() then
-		managers.enemy:add_delayed_clbk("_radio_chatter_clbk", self._radio_clbk, Application:time() + math.random(0, 20))
-	else
-		local commonradiotime = math.random(10, 15)
-		managers.enemy:add_delayed_clbk("_radio_chatter_clbk", self._radio_clbk, Application:time() + math.lerp(commonradiotime, 1, self._drama_data.amount))
-	end
-end
-
 function GroupAIStateBase:chk_area_leads_to_enemy(start_nav_seg_id, test_nav_seg_id, enemy_is_criminal)
 	local enemy_areas = {}
 
