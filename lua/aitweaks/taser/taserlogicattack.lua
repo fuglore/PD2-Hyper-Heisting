21d252c146d6f092e95421becb3ddfa441e00857
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-01-10 22:09:26 -0300

    forgot one little thing

--- lua/aitweaks/taser/taserlogicattack.lua
+++ lua/aitweaks/taser/taserlogicattack.lua
@@ -312,3 +312,11 @@ function TaserLogicAttack._upd_enemy_detection(data)
 
 	TaserLogicAttack._upd_aim(data, my_data, new_reaction)
 end
+
+function TaserLogicAttack._chk_play_charge_weapon_sound(data, my_data, focus_enemy)
+	--if not my_data.tasing and (not my_data.last_charge_snd_play_t or data.t - my_data.last_charge_snd_play_t > 30) and focus_enemy.verified_dis < 2000 and math.abs(data.m_pos.z - focus_enemy.m_pos.z) < 300 then
+		--my_data.last_charge_snd_play_t = data.t
+
+		--data.unit:sound():play("taser_charge", nil, true)
+	--end
+end
