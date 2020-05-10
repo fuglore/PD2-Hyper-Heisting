50f8927edf4787b3777f9a61ac1c17a883755068
Neslon-Poggers <nanny01023@gmail.com>
2020-05-09 21:33:11 -0400

    buge repair

--- lua/menunodegui.lua
+++ lua/menunodegui.lua
@@ -11,21 +11,24 @@ local warned_mods = {
 Hooks:PostHook(MenuNodeGui,"_setup_item_rows","shin_mememode_safety_notif",function(self,node,...)
     local title = "HYPER HEISTING MEME MODE SAFETY FEATURE ACTIVATED"
     local desc = "Caution! You have the following AI-changing mods installed, which may conflict with Hyper Heisting:\n"
-    
+    local has_any
     for key,mod_name in pairs(warned_mods) do 
         if rawget(_G,key) then 
+            has_any = true
             desc = desc .. "\n" .. mod_name
         end
     end
-    
-    QuickMenu:new(
-        title,
-        desc,
-        {
+	
+    if has_any then 
+        QuickMenu:new(
+            title,
+            desc,
             {
-                text = "ok FINE MOM GOD",
-                is_cancel_button = true
+                {
+                    text = "ok fine mom GOD",
+                    is_cancel_button = true
+                }
             }
-        }
-    ,true)
+        ,true)
+    end
 end)
