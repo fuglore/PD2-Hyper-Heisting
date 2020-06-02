1d5a9f09896c566347f2084fe7f1bada4d4af5c7
Neslon-Poggers <nanny01023@gmail.com>
2020-06-02 18:17:12 -0400

    asdfadjsdjajsjdsjsajj

--- lua/aitweaks/groupaistatebase.lua
+++ lua/aitweaks/groupaistatebase.lua
@@ -1,4 +1,6 @@
 -- Andole's dozer spawncap fix
+GroupAIStateBase._nr_important_cops = 6969
+
 local origfunc3 = GroupAIStateBase._init_misc_data
 function GroupAIStateBase:_init_misc_data(...)
 	origfunc3(self, ...)
@@ -153,74 +155,6 @@ function GroupAIStateBase:chk_random_drama_comment()
 	end
 	
 	self._rolled_dramatalk_chance = true
-end
-
-function GroupAIStateBase:set_importance_weight(u_key, wgt_report)
-	if #wgt_report == 0 then
-		return
-	end
-
-	local t_rem = table.remove
-	local t_ins = table.insert
-	local max_nr_imp = 6969 --sex
-	local imp_adj = 0
-	local criminals = self._player_criminals
-	local cops = self._police
-
-	for i_dis_rep = #wgt_report - 1, 1, -2 do
-		local c_key = wgt_report[i_dis_rep]
-		local c_dis = wgt_report[i_dis_rep + 1]
-		local c_record = criminals[c_key]
-		local imp_enemies = c_record.important_enemies
-		local imp_dis = c_record.important_dis
-		local was_imp = nil
-
-		for i_imp = #imp_enemies, 1, -1 do
-			if imp_enemies[i_imp] == u_key then
-				table.remove(imp_enemies, i_imp)
-				table.remove(imp_dis, i_imp)
-
-				was_imp = true
-
-				break
-			end
-		end
-
-		local i_imp = #imp_dis
-
-		while i_imp > 0 do
-			if imp_dis[i_imp] <= c_dis then
-				break
-			end
-
-			i_imp = i_imp - 1
-		end
-
-		if i_imp < max_nr_imp then
-			i_imp = i_imp + 1
-
-			while max_nr_imp <= #imp_enemies do
-				local dump_e_key = imp_enemies[#imp_enemies]
-
-				self:_adjust_cop_importance(dump_e_key, -1)
-				t_rem(imp_enemies)
-				t_rem(imp_dis)
-			end
-
-			t_ins(imp_enemies, i_imp, u_key)
-			t_ins(imp_dis, i_imp, c_dis)
-
-			if not was_imp then
-				imp_adj = imp_adj + 1
-			end
-		elseif was_imp then
-			imp_adj = imp_adj - 1
-		end
-	end
-
-	if imp_adj ~= 0 then
-		self:_adjust_cop_importance(u_key, imp_adj)
-	end
 end
 
 function GroupAIStateBase:chk_taunt()
