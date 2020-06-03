1b2199dbc87aa8d580e5bf15eed19d42c35d8ab1
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-03 00:17:23 -0300

    Saw code is now less (vanilla) stupid
    Should only consume ammo when hitting:
    -Enemies or civs (need to be alive)
    -Walls and vehicles
    -Shield objects
    
    It should also go through body armor (please change this at the top if not wanted) and not knock Shields around as it ends up being annoying.

--- lua/sawweaponbase.lua
+++ lua/sawweaponbase.lua
@@ -16,6 +16,14 @@ if PD2THHSHIN and PD2THHSHIN:IsOverhaulEnabled() then
 	local mvec_to = Vector3()
 	local mvec_spread_direction = Vector3()
 
+	local original_init = SawWeaponBase.init
+	function SawWeaponBase:init(unit)
+		original_init(self, unit)
+
+		self._shield_knock = false
+		self._use_armor_piercing = true
+	end
+
 	--i hate doing this so much but i couldn't figure out a proper way to posthook this. hopefully someone else can figure out how but for now i guess i'll just die
 	function SawWeaponBase:fire(from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul, target_unit)
 		if self:get_ammo_remaining_in_clip() == 0 then
@@ -23,41 +31,43 @@ if PD2THHSHIN and PD2THHSHIN:IsOverhaulEnabled() then
 		end
 
 		local user_unit = self._setup.user_unit
-		local ray_res, hit_something = self:_fire_raycast(user_unit, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul, target_unit)
+		local ray_res, hit_something, drain_ammo = self:_fire_raycast(user_unit, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul, target_unit)
 
 		if hit_something then
 			self:_start_sawing_effect()
 
-			local ammo_usage = 5
+			if drain_ammo and not managers.player:has_active_temporary_property("bullet_storm") then
+				local ammo_usage = 5
 
-			if ray_res.hit_enemy then
-				ammo_usage = 15
-			end
+				if ray_res.hit_enemy then
+					if managers.player:has_category_upgrade("saw", "enemy_slicer") then
+						ammo_usage = 0
+					else
+						ammo_usage = 15
+					end
+				end
 
-			ammo_usage = ammo_usage + math.ceil(math.random() * 10)
+				if ammo_usage > 0 then
+					ammo_usage = ammo_usage + math.ceil(math.random() * 10)
 
-			if managers.player:has_category_upgrade("saw", "consume_no_ammo_chance") then
-				local roll = math.rand(1)
-				local chance = managers.player:upgrade_value("saw", "consume_no_ammo_chance", 0)
+					if managers.player:has_category_upgrade("saw", "consume_no_ammo_chance") then
+						local roll = math.rand(1)
+						local chance = managers.player:upgrade_value("saw", "consume_no_ammo_chance", 0)
 
-				if roll < chance then
-					ammo_usage = 0
+						if roll < chance then
+							ammo_usage = 0
+						end
+					end
+
+					if ammo_usage > 0 then
+						ammo_usage = math.min(ammo_usage, self:get_ammo_remaining_in_clip())
+
+						self:set_ammo_remaining_in_clip(math.max(self:get_ammo_remaining_in_clip() - ammo_usage, 0))
+						self:set_ammo_total(math.max(self:get_ammo_total() - ammo_usage, 0))
+						self:_check_ammo_total(user_unit)
+					end
 				end
 			end
-
-			if managers.player:has_active_temporary_property("bullet_storm") then
-				ammo_usage = 0
-			end
-			
-			if managers.player:has_category_upgrade("saw", "enemy_slicer") and ray_res.hit_enemy then
-				ammo_usage = 0
-			end
-
-			ammo_usage = math.min(ammo_usage, self:get_ammo_remaining_in_clip())
-
-			self:set_ammo_remaining_in_clip(math.max(self:get_ammo_remaining_in_clip() - ammo_usage, 0))
-			self:set_ammo_total(math.max(self:get_ammo_total() - ammo_usage, 0))
-			self:_check_ammo_total(user_unit)
 		else
 			self:_stop_sawing_effect()
 		end
@@ -76,91 +86,109 @@ if PD2THHSHIN and PD2THHSHIN:IsOverhaulEnabled() then
 
 		return ray_res
 	end
-	
-	local function ray_table_contains(table, unit)
-		for i, hit in pairs(table) do
-			if hit.unit == unit then
-				return true
-			end
-		end
 
-		return false
-	end
+	local mvec_to = Vector3()
+	local mvec_spread_direction = Vector3()
 
-	local function ray_copy(table, ray)
-		for i, hit in pairs(table) do
-			if hit.unit == ray.unit then
-				hit.body = ray.body
-				hit.distance = ray.distance
-
-				mvector3.set(hit.hit_position, ray.hit_position)
-				mvector3.set(hit.normal, ray.normal)
-				mvector3.set(hit.position, ray.position)
-				mvector3.set(hit.ray, ray.ray)
-			end
-		end
-	end	
-	
 	function SawWeaponBase:_fire_raycast(user_unit, from_pos, direction, dmg_mul, shoot_player, spread_mul, autohit_mul, suppr_mul)
 		local result = {}
 		local hit_unit = nil
+		local valid_hit = false
+		local drain_ammo = false
 		from_pos = self._obj_fire:position()
 		direction = self._obj_fire:rotation():y()
 
-		mvec3_add(from_pos, direction * -30)
+		mvector3.add(from_pos, direction * -30)
 		mvector3.set(mvec_spread_direction, direction)
 		mvector3.set(mvec_to, mvec_spread_direction)
-		mvector3.multiply(mvec_to, 280)
+		mvector3.multiply(mvec_to, 100)
 		mvector3.add(mvec_to, from_pos)
 
 		local damage = self:_get_current_damage(dmg_mul)
-		local valid_hit = false
-		local col_ray = nil
-		local true_slot_mask = managers.slot:get_mask("world_geometry", "vehicles", "enemy_shield_check", "enemies", "civilians")
-		if self._saw_through_shields then
-			local hits = {}
-			col_ray = World:raycast_all("ray", from_pos, mvec_to, "slot_mask", true_slot_mask, "ignore_unit", self._setup.ignore_units, "ray_type", "body bullet lock")
 
-			for i, hit in ipairs(col_ray) do
-				local is_shield = hit.unit:in_slot(8) and alive(hit.unit:parent())
-				local is_enemy = hit.unit:character_damage() ~= nil
+		local shield_mask = managers.slot:get_mask("enemy_shield_check")
+		local enemy_mask = managers.slot:get_mask("enemies")
+		local civilian_mask = managers.slot:get_mask("civilians")
+		local wall_mask = managers.slot:get_mask("world_geometry", "vehicles")
 
-				if not ray_table_contains(hits, hit.unit) then
-					table.insert(hits, hit)
-				elseif hit.unit:character_damage() and hit.unit:character_damage().is_head and hit.unit:character_damage():is_head(hit.body) then
-					ray_copy(hits, hit)
+		local ray_hits = World:raycast_all("ray", from_pos, mvec_to, "slot_mask", self._bullet_slotmask, "ignore_unit", self._setup.ignore_units, "ray_type", "body bullet lock")
+		local units_hit = {}
+		local unique_hits = {}
+
+		for i, hit in ipairs(ray_hits) do
+			if not units_hit[hit.unit:key()] then
+				units_hit[hit.unit:key()] = true
+				unique_hits[#unique_hits + 1] = hit
+				hit.hit_position = hit.position
+				drain_ammo = drain_ammo or hit.unit:in_slot(shield_mask) or hit.unit:in_slot(enemy_mask) or hit.unit:in_slot(civilian_mask) or hit.unit:in_slot(wall_mask)
+
+				if hit.unit:in_slot(shield_mask) then
+					if not self._saw_through_shields then
+						break
+					end
+				elseif hit.unit:in_slot(enemy_mask) or hit.unit:in_slot(wall_mask) then
+					break
 				end
 			end
-
-			for i, hit in pairs(hits) do
-				hit_unit = SawHit:on_collision(hit, self._unit, user_unit, damage, direction)
-			end
-
-			valid_hit = #col_ray > 0
-		else
-			col_ray = World:raycast("ray", from_pos, mvec_to, "slot_mask", true_slot_mask, "ignore_unit", self._setup.ignore_units, "ray_type", "body bullet lock")
-
-			if col_ray then
-				hit_unit = SawHit:on_collision(col_ray, self._unit, user_unit, damage, direction)
-				valid_hit = true
-			end
 		end
+
+		for i, hit in ipairs(unique_hits) do
+			hit_unit = SawHit:on_collision(hit, self._unit, user_unit, damage, direction)
+		end
+
+		valid_hit = #ray_hits > 0
 
 		result.hit_enemy = hit_unit
 
 		if self._alert_events then
 			result.rays = {
-				col_ray
+				ray_hits
 			}
 		end
 
-		if col_ray then
+		if ray_hits then
 			managers.statistics:shot_fired({
 				hit = true,
 				weapon_unit = self._unit
 			})
 		end
 
-		return result, valid_hit
-	end	
+		return result, valid_hit, drain_ammo
+	end
+
+	function SawHit:on_collision(col_ray, weapon_unit, user_unit, damage)
+		local hit_unit = col_ray.unit
+
+		if self._saw_through_shields and hit_unit and hit_unit.base and hit_unit:base() and hit_unit:base().has_tag and hit_unit:base():has_tag("tank") then
+			damage = damage + 50
+		end
+
+		local result = InstantBulletBase.on_collision(self, col_ray, weapon_unit, user_unit, damage)
+
+		if hit_unit:damage() and col_ray.body:extension() and col_ray.body:extension().damage then
+			damage = math.clamp(damage * managers.player:upgrade_value("saw", "lock_damage_multiplier", 1) * 4, 0, 200)
+
+			col_ray.body:extension().damage:damage_lock(user_unit, col_ray.normal, col_ray.position, col_ray.direction, damage)
+
+			if hit_unit:id() ~= -1 then
+				managers.network:session():send_to_peers_synched("sync_body_damage_lock", col_ray.body, damage)
+			end
+		end
+
+		return result
+	end
+
+	function SawHit:play_impact_sound_and_effects(weapon_unit, col_ray)
+		local decal = "saw"
+
+		if col_ray.unit:character_damage() and not col_ray.unit:character_damage()._no_blood then
+			decal = nil
+		end
+
+		managers.game_play_central:play_impact_sound_and_effects({
+			decal = decal,
+			no_sound = true,
+			col_ray = col_ray
+		})
+	end
 end
