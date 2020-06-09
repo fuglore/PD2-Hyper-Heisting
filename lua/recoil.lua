5d5838e17c8a49b1c63b0cfd192b544cb5df3dbc
Neslon-Poggers <nanny01023@gmail.com>
2020-06-08 23:13:58 -0400

    MY FUCKING GOD DAWG

--- lua/recoil.lua
+++ lua/recoil.lua
@@ -1,11 +1,13 @@
-function FPCameraPlayerBase:recoil_kick(up, down, left, right)
-	local v = math.lerp(up, down, math.random())
-	self._recoil_kick.accumulated = ((self._recoil_kick.accumulated or 0) + v)
-	local h = math.lerp(left, right, math.random())
-	self._recoil_kick.h.accumulated = ((self._recoil_kick.h.accumulated or 0) + h)
-end
-function FPCameraPlayerBase:stop_shooting(wait)
-	self._recoil_kick.to_reduce = 0
-	self._recoil_kick.h.to_reduce = 0
-	self._recoil_wait = nil
+if PD2THHSHIN and PD2THHSHIN:IsOverhaulEnabled() then
+	function FPCameraPlayerBase:recoil_kick(up, down, left, right)
+		local v = math.lerp(up, down, math.random())
+		self._recoil_kick.accumulated = ((self._recoil_kick.accumulated or 0) + v)
+		local h = math.lerp(left, right, math.random())
+		self._recoil_kick.h.accumulated = ((self._recoil_kick.h.accumulated or 0) + h)
+	end
+	function FPCameraPlayerBase:stop_shooting(wait)
+		self._recoil_kick.to_reduce = 0
+		self._recoil_kick.h.to_reduce = 0
+		self._recoil_wait = nil
+	end
 end
