3373399e9df2fa885f98feb36dd30b3da8e6785b
Reeeno <40873080+EvilBobarino@users.noreply.github.com>
2020-06-24 01:54:04 +0100

    coplogicswag

--- lua/aitweaks/coplogictravel.lua
+++ lua/aitweaks/coplogictravel.lua
@@ -908,7 +908,7 @@ function CopLogicTravel.action_complete_clbk(data, action)
 			-- CopLogicTravel._upd_combat_movement(data)
 		end
 	elseif action_type == "walk" then
-		if action:expired() and not my_data.starting_advance_action and my_data.coarse_path_index and not my_data.has_old_action and my_data.advancing then
+		if action:expired() and my_data.coarse_path and not my_data.starting_advance_action and my_data.coarse_path_index and not my_data.has_old_action and my_data.advancing then
 			my_data.coarse_path_index = my_data.coarse_path_index + 1
 
 			if my_data.coarse_path_index > #my_data.coarse_path then
@@ -938,8 +938,14 @@ function CopLogicTravel.action_complete_clbk(data, action)
 				local high_ray = CopLogicTravel._chk_cover_height(data, my_data.best_cover[1], data.visibility_slotmask)
 				my_data.best_cover[4] = high_ray
 				my_data.in_cover = true
-				local cover_wait_time = my_data.coarse_path_index == #my_data.coarse_path - 1 and 0.3 or 0.6 + 0.4 * math.random()
-
+				local cover_wait_time = 0
+				
+				if my_data.coarse_path then
+				    cover_wait_time = my_data.coarse_path_index == #my_data.coarse_path - 1 and 0.3 or 0.6 + 0.4 * math.random()
+				else
+					cover_wait_time = 0.6 + 0.4 * math.random()
+  			    end
+				
 				if not CopLogicTravel._chk_close_to_criminal(data, my_data) then
 					cover_wait_time = 0
 				end
@@ -1605,6 +1611,82 @@ function CopLogicTravel._chk_start_pathing_to_next_nav_point(data, my_data)
 	data.unit:brain():search_for_path(my_data.advance_path_search_id, to_pos, prio, nil, nav_segs)
 end
 
+function CopLogicTravel.upd_advance(data)
+	local unit = data.unit
+	local my_data = data.internal_data
+	local objective = data.objective
+	local t = TimerManager:game():time()
+	data.t = t
+
+	if my_data.has_old_action then
+		CopLogicAttack._upd_stop_old_action(data, my_data)
+	elseif my_data.warp_pos then
+		local action_desc = {
+			body_part = 1,
+			type = "warp",
+			position = mvector3.copy(objective.pos),
+			rotation = objective.rot
+		}
+
+		if unit:movement():action_request(action_desc) then
+			CopLogicTravel._on_destination_reached(data)
+		end
+	elseif my_data.advancing then
+		if my_data.coarse_path then
+			if my_data.announce_t and my_data.announce_t < t then
+				CopLogicTravel._try_anounce(data, my_data)
+			end
+            
+			if data.is_converted and not data.converted_has_wiped_coarse then
+			    my_data.coarse_path = nil
+				data.converted_has_wiped_coarse = true
+			end
+			
+			CopLogicTravel._chk_stop_for_follow_unit(data, my_data)
+
+			if my_data ~= data.internal_data then
+				return
+			end
+		end
+	elseif my_data.advance_path then
+		CopLogicTravel._chk_begin_advance(data, my_data)
+
+		if my_data.advancing and my_data.path_ahead then
+			CopLogicTravel._check_start_path_ahead(data)
+		end
+	elseif my_data.processing_advance_path or my_data.processing_coarse_path then
+		CopLogicTravel._upd_pathing(data, my_data)
+
+		if my_data ~= data.internal_data then
+			return
+		end
+	elseif my_data.cover_leave_t then
+		if not my_data.turning and not unit:movement():chk_action_forbidden("walk") and not data.unit:anim_data().reload then
+			if my_data.cover_leave_t < t then
+				my_data.cover_leave_t = nil
+			elseif data.attention_obj and AIAttentionObject.REACT_SCARED <= data.attention_obj.reaction and (not my_data.best_cover or not my_data.best_cover[4]) and not unit:anim_data().crouch and (not data.char_tweak.allowed_poses or data.char_tweak.allowed_poses.crouch) then
+				CopLogicAttack._chk_request_action_crouch(data)
+			end
+		end
+	elseif objective and (objective.nav_seg or objective.type == "follow") then
+		if my_data.coarse_path then
+			if my_data.coarse_path_index == #my_data.coarse_path then
+				CopLogicTravel._on_destination_reached(data)
+
+				return
+			else
+				CopLogicTravel._chk_start_pathing_to_next_nav_point(data, my_data)
+			end
+		else
+			CopLogicTravel._begin_coarse_pathing(data, my_data)
+		end
+	else
+		CopLogicBase._exit(data.unit, "idle")
+
+		return
+	end
+end
+
 function CopLogicTravel._chk_stop_for_follow_unit(data, my_data)
 	local objective = data.objective
 
@@ -1620,7 +1702,7 @@ function CopLogicTravel._chk_stop_for_follow_unit(data, my_data)
 
 	local follow_unit_nav_seg = data.objective.follow_unit:movement():nav_tracker():nav_segment()
 	
-	if not follow_unit_nav_seg or not my_data.coarse_path or not my_data.coarse_path_index then 
+	if not follow_unit_nav_seg or not my_data.coarse_path or not my_data.coarse_path_index or not my_data.coarse_path[my_data.coarse_path_index + 1][1] then 
 		log("FUCKINGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 		return
 	end
