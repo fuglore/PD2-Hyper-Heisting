72649a0acff807082d2c36b313071169cb06b129
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-06-30 21:42:37 -0300

    Update unitnetworkhandler.lua

--- lua/unitnetworkhandler.lua
+++ lua/unitnetworkhandler.lua
@@ -1,4 +1,8 @@
 function UnitNetworkHandler:sync_add_doted_enemy(enemy_unit, variant, weapon_unit, dot_length, dot_damage, user_unit, is_molotov_or_hurt_animation, rpc)
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) then
+		return
+	end
+
 	if variant == 0 then
 		managers.fire:sync_add_fire_dot(enemy_unit, nil, weapon_unit, dot_length, dot_damage, user_unit, is_molotov_or_hurt_animation)
 	else
@@ -30,39 +34,39 @@ function UnitNetworkHandler:sync_add_doted_enemy(enemy_unit, variant, weapon_uni
 end
 
 function UnitNetworkHandler:action_spooc_start(unit, target_u_pos, flying_strike, action_id)
-    if not self._verify_character(unit) or not self._verify_gamestate(self._gamestate_filter.any_ingame) then
-        return
-    end
+	if not self._verify_gamestate(self._gamestate_filter.any_ingame) or not self._verify_character(unit) then
+		return
+	end
 
-    local action_desc = {
-        block_type = "walk",
-        type = "spooc",
-        path_index = 1,
-        body_part = 1,
-        nav_path = {
-            unit:position()
-        },
-        target_u_pos = target_u_pos,
-        flying_strike = flying_strike,
-        action_id = action_id,
-        blocks = {
-            act = -1,
-            turn = -1,
-            walk = -1
-        }
-    }
+	local action_desc = {
+		block_type = "walk",
+		type = "spooc",
+		path_index = 1,
+		body_part = 1,
+		nav_path = {
+			unit:position()
+		},
+		target_u_pos = target_u_pos,
+		flying_strike = flying_strike,
+		action_id = action_id,
+		blocks = {
+			idle = -1,
+			act = -1,
+			turn = -1,
+			walk = -1
+		}
+	}
 
-    if flying_strike then
-        action_desc.blocks.idle = -1
-        action_desc.blocks.light_hurt = -1
-        action_desc.blocks.heavy_hurt = -1
-        action_desc.blocks.fire_hurt = -1
-        action_desc.blocks.hurt = -1
-        action_desc.blocks.expl_hurt = -1
-        action_desc.blocks.taser_tased = -1
-    end
+	if flying_strike then
+		action_desc.blocks.light_hurt = -1
+		action_desc.blocks.heavy_hurt = -1
+		action_desc.blocks.fire_hurt = -1
+		action_desc.blocks.hurt = -1
+		action_desc.blocks.expl_hurt = -1
+		action_desc.blocks.taser_tased = -1
+	end
 
-    unit:movement():action_request(action_desc)
+	unit:movement():action_request(action_desc)
 end
 
 function UnitNetworkHandler:action_aim_state(unit, state)
