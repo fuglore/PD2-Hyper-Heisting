f6c4c937258f287eaa1fc53286e1abad732e6290
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-07-04 01:18:36 -0300

    new early game night env

--- lua/CoreScriptData.lua
+++ lua/CoreScriptData.lua
@@ -1,5 +1,35 @@
 local mod_path = tostring(PD2THHSHIN._mod_path)
 
+Hooks:Add("BeardLibPreProcessScriptData", "HHCreateEnvironment", function(PackManager, path, raw_data)
+    if managers.dyn_resource then
+        local skies = {
+            "sky_1930_twillight",
+			"sky_1930_sunset_heavy_clouds",
+            "sky_1846_low_sun_nice_clouds",
+            "sky_0902_overcast",
+			"sky_1345_clear_sky",
+			"sky_0200_night_moon_stars",
+			"sky_2000_twilight_mad",
+			"sky_2100_moon",
+			"sky_1008_cloudy",
+			"sky_0927_whispy_clouds",
+			"sky_2335_night_moon",
+			"sky_2100_moon",
+			"sky_1313_cloudy_dark",
+			"sky_2003_dusk_blue_high_color_scale",
+			"sky_2335_night_moon"
+			
+        }
+        for _, sky in ipairs(skies) do
+            if not managers.dyn_resource:has_resource(Idstring("scene"), Idstring("core/environments/skies/" .. sky .. "/" .. sky), managers.dyn_resource.DYN_RESOURCES_PACKAGE) then
+                --restoration.log_shit("not loaded")
+                managers.dyn_resource:load(Idstring("scene"), Idstring("core/environments/skies/" .. sky .. "/" .. sky), managers.dyn_resource.DYN_RESOURCES_PACKAGE, nil)
+            end
+        end
+    end
+end)
+	
+
 Hooks:Add("BeardLibCreateScriptDataMods", "CustomEnvCallBeardLibSequenceFuncs", function()
 	local difficulty = Global.game_settings and Global.game_settings.difficulty or "normal"
 	local diff_index = tweak_data:difficulty_to_index(difficulty)
@@ -7,6 +37,7 @@ Hooks:Add("BeardLibCreateScriptDataMods", "CustomEnvCallBeardLibSequenceFuncs", 
 	local sex = Global.level_data and Global.level_data.level_id
 	
 	BeardLib:ReplaceScriptData(mod_path .. "scriptdata/pd2_env_day_hh.custom_xml", "custom_xml", "environments/pd2_env_mid_day/pd2_env_mid_day", "environment")
+	BeardLib:ReplaceScriptData(mod_path .. "scriptdata/pd2_env_night_hh.custom_xml", "custom_xml", "environments/pd2_env_night/pd2_env_night", "environment")
 	
 	if diff_index == 8 and sex == "friend" then
 		BeardLib:ReplaceScriptData(mod_path .. "scriptdata/shitfaceenvironment.custom_xml", "custom_xml", "environments/pd2_friend/pd2_friend", "environment")
