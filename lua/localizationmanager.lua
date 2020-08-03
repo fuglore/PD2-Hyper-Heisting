805ecf9203ee3f5ffb19bdca9b5bf881d3d01eee
bad@names <averygoodname77@gmail.com>
2020-08-02 22:44:12 -0400

    loc change
    cuff punch

--- lua/localizationmanager.lua
+++ lua/localizationmanager.lua
@@ -61,7 +61,7 @@
 		["hud_assault_FG_cover59"] = "IT'S THE WAY U MOVE, TO THE KILLER GROOVE",
 		["hud_assault_FG_cover60"] = "FIGHT ON",
 		["hud_assault_FG_cover61"] = "STUPID COPS, I'M FUCKIN' BALLIN",
-		["hud_assault_FG_cover62"] = "DID HE JUST WALK UP SLOWLY AND CUFF HIM?",
+		["hud_assault_FG_cover62"] = "DID HE JUST WALK UP SLOWLY AND CUFF PUNCH?",
 		["hud_assault_FG_cover63"] = "#verifyvenuz",
 		["hud_assault_faction_federales"] = "VS. LOS FEDERALES",
 		["hud_assault_faction_swat"] = "VS. SWAT TEAM",
