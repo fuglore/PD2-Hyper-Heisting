7a74fa9a13c4c70b6c65dc99d3e707cd48c1d5d6
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-08-05 13:25:30 -0300

    Fixed magazine data crash without banning people
    full speed crap

--- lua/aitweaks/player/huskplayermovement.lua
+++ lua/aitweaks/player/huskplayermovement.lua
@@ -4,6 +4,12 @@ local mvec3_z = mvector3.z
 local mvec3_cpy = mvector3.copy
 local mvec3_dist_sq = mvector3.distance_sq
 local mvec3_lerp = mvector3.lerp
+local mvec3_add = mvector3.add
+local mvec3_sub = mvector3.subtract
+local mvec3_mul = mvector3.multiply
+
+local tmp_vec1 = Vector3()
+local tmp_vec2 = Vector3()
 
 local mrot_look = mrotation.set_look_at
 
@@ -13,6 +19,9 @@ local math_point_on_line = math.point_on_line
 
 local table_insert = table.insert
 local table_remove = table.remove
+
+local left_hand_str = Idstring("LeftHandMiddle2")
+local right_hand_str = Idstring("RightHandMiddle2")
 
 local post_init_original = HuskPlayerMovement.post_init
 function HuskPlayerMovement:post_init()
@@ -218,6 +227,108 @@ function HuskPlayerMovement:_update_zipline_sled(t, dt)
 
 			zipline:update_and_get_pos_at_time_linear(math_clamp(t, 0, 1))
 		end
+	end
+end
+
+function HuskPlayerMovement:anim_clbk_spawn_dropped_magazine()
+	if not self:allow_dropped_magazines() then
+		return
+	end
+
+	local equipped_weapon = self._unit:inventory():equipped_unit()
+
+	if alive(equipped_weapon) and not equipped_weapon:base()._assembly_complete then
+		return
+	end
+
+	local ref_unit = nil
+	local allow_throw = true
+
+	if not self._magazine_data then
+		local w_td_crew = self:_equipped_weapon_crew_tweak_data()
+
+		if not w_td_crew or not w_td_crew.pull_magazine_during_reload then
+			return
+		end
+
+		self:anim_clbk_show_magazine_in_hand()
+
+		if not self._magazine_data then --prevent the function from going any further if no data was defined with self:anim_clbk_show_magazine_in_hand()
+			return
+		elseif not alive(self._magazine_data.unit) then --prevent the function from going any further + delete the data if the magazine unit somehow didn't spawn
+			self._magazine_data = nil
+
+			return
+		end
+
+		local attach_bone = nil
+
+		if not self._primary_hand or self._primary_hand == 0 then
+			attach_bone = left_hand_str
+		else
+			attach_bone = right_hand_str
+		end
+
+		local bone_hand = self._unit:get_object(attach_bone)
+
+		if bone_hand then
+			mvec3_set(tmp_vec1, self._magazine_data.unit:position())
+			mvec3_sub(tmp_vec1, self._magazine_data.unit:oobb():center())
+			mvec3_add(tmp_vec1, bone_hand:position())
+			self._magazine_data.unit:set_position(tmp_vec1)
+		end
+
+		ref_unit = self._magazine_data.part_unit
+		allow_throw = false
+	end
+
+	if self._magazine_data and alive(self._magazine_data.unit) then
+		ref_unit = ref_unit or self._magazine_data.unit
+
+		self._magazine_data.unit:set_visible(false)
+
+		local pos = ref_unit:position()
+		local rot = ref_unit:rotation()
+		local dropped_mag = self:_spawn_magazine_unit(self._magazine_data.id, self._magazine_data.name, pos, rot)
+
+		self:_set_unit_bullet_objects_visible(dropped_mag, self._magazine_data.bullets, false)
+
+		local mag_size = self._magazine_data.weapon_data.pull_magazine_during_reload
+
+		if type(mag_size) ~= "string" then
+			mag_size = "medium"
+		end
+
+		mvec3_set(tmp_vec1, ref_unit:oobb():center())
+		mvec3_sub(tmp_vec1, pos)
+		mvec3_set(tmp_vec2, pos)
+		mvec3_add(tmp_vec2, tmp_vec1)
+
+		local dropped_col = World:spawn_unit(HuskPlayerMovement.magazine_collisions[mag_size][1], tmp_vec2, rot)
+
+		dropped_col:link(HuskPlayerMovement.magazine_collisions[mag_size][2], dropped_mag)
+
+		if allow_throw then
+			if self._left_hand_direction then
+				local throw_force = 10
+
+				mvec3_set(tmp_vec1, self._left_hand_direction)
+				mvec3_mul(tmp_vec1, self._left_hand_velocity or 3)
+				mvec3_mul(tmp_vec1, math_random(25, 45))
+				mvec3_mul(tmp_vec1, -1)
+				dropped_col:push(throw_force, tmp_vec1)
+			end
+		else
+			local throw_force = 10
+			local _t = (self._reload_speed_multiplier or 1) - 1
+
+			mvec3_set(tmp_vec1, equipped_weapon:rotation():z())
+			mvec3_mul(tmp_vec1, math_lerp(math_random(65, 80), math_random(140, 160), _t))
+			mvec3_mul(tmp_vec1, math_random() < 0.0005 and 10 or -1) --0.0005% chance to send the magazine flying upwards (it's normally like this, just pointing it out)
+			dropped_col:push(throw_force, tmp_vec1)
+		end
+
+		managers.enemy:add_magazine(dropped_mag, dropped_col)
 	end
 end
 
