02a02b87bd39eb86beee169252e353d403d0fc3d
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-09-05 14:06:27 +0100

    Improved heist specific music code

--- lua/musicmanager.lua
+++ lua/musicmanager.lua
@@ -1,8 +1,5 @@
 -- CREDIT: TEST1
-local jukebox_heist_specific_orig = MusicManager.jukebox_heist_specific
-
-function MusicManager:jukebox_heist_specific()
-    jukebox_heist_specific_orig(self)
+Hooks:PostHook(MusicManager, "jukebox_heist_specific", "HH_MusicManagerJukeBoxHeistSpecific", function(self)
 	
     if not Global.music_manager.track_attachment.flat_hh then
         Global.music_manager.track_attachment.flat_hh = "track_47_gen"
@@ -16,7 +13,7 @@ function MusicManager:jukebox_heist_specific()
         Global.music_manager.track_attachment.nmh_hyper = "track_63"
     end
     
-    local job_data = Global.job_manager.current_job
+    -- local job_data = Global.job_manager.current_job
 	
     if managers.job:current_level_id() == "flat_hh" then
         return self:track_attachment("flat_hh") or "all"
@@ -29,28 +26,4 @@ function MusicManager:jukebox_heist_specific()
     if managers.job:current_level_id() == "nmh_hyper" then
         return self:track_attachment("nmh_hyper") or "all"
     end
-
-	local job_data = Global.job_manager.current_job
-
-	if job_data then
-		local job_tweak = tweak_data.narrative:job_data(job_data.job_id)
-
-		if job_tweak then
-			local track_data = job_tweak.name_id .. (job_data.stages > 1 and job_data.current_stage or "")
-
-			return self:track_attachment(track_data) or "all"
-		end
-	end
-
-	if managers.crime_spree:is_active() then
-		local narrative_data, day, variant = managers.crime_spree:get_narrative_tweak_data_for_mission_level(managers.crime_spree:current_mission())
-
-		if narrative_data then
-			local track_data = narrative_data.name_id .. ((narrative_data.stages or 1) > 1 and tostring(day) or "")
-
-			return self:track_attachment(track_data) or "all"
-		end
-	end
-
-	return "all"
-end
+end)
