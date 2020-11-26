ef54bdf5d010b8f3604ea2e46c4db523c8d386f5
Fuglore <44823551+fuglore@users.noreply.github.com>
2020-11-25 21:08:18 -0300

    whoops

--- lua/aitweaks/civilians/civilianlogicflee.lua
+++ lua/aitweaks/civilians/civilianlogicflee.lua
@@ -173,7 +173,7 @@ function CivilianLogicFlee.on_rescue_SO_administered(ignore_this, data, receiver
 	my_data.rescuer = receiver_unit
 	my_data.rescue_SO_id = nil
 
-	receiver_unit:sound():say("c01", true) --"We'll come to you!"
+	receiver_unit:sound():say("cr1", true) --"We'll come to you!"
 
 	managers.groupai:state():unregister_rescueable_hostage(data.key)
 end
