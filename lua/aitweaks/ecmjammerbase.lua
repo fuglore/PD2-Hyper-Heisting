38e34e63cf421bfa6a6ea7e533de00202bf39ef8
Hoxi <44041474+hoxiwoopboop@users.noreply.github.com>
2020-11-27 01:51:33 -0300

    Fixed a typo + ECM feedback for clients
    The self._position and self._rotation mutables are never updated. This leads to:
    -ECMs placed locally through the server end up being busted if the body the ECM is attached to moves or rotates.
    -ECMs placement requested by clients ends up using {0, 0, 0} as the initial position and rotation for every peer, host included. So it obviously won't even matter if the body moves or rotates and they're even more busted.

--- lua/aitweaks/ecmjammerbase.lua
+++ lua/aitweaks/ecmjammerbase.lua
@@ -7,15 +7,9 @@ local world_g = World
 local ipairs_g = ipairs
 
 function ECMJammerBase._detect_and_give_dmg(from_pos, device_unit, user_unit, range)
-	--[[local enemies_in_range = nil
-
-	if device_unit then
-		enemies_in_range = world_g:find_units_quick("sphere", from_pos, range, managers.slot:get_mask("enemies"))
-	else
-		enemies_in_range = world_g:find_units_quick("all", managers.slot:get_mask("enemies")) --affect enemies on the entire map, to match the Pocket ECM's ingame description for meme purposes
-	end]]
-
 	local enemies_in_range = world_g:find_units_quick("sphere", from_pos, range, managers.slot:get_mask("enemies"))
+	local attacker = alive(user_unit) and user_unit
+	local weapon = alive(device_unit) and device_unit
 
 	for _, enemy in ipairs_g(enemies_in_range) do
 		local dmg_ext = enemy:character_damage()
@@ -23,23 +17,23 @@ function ECMJammerBase._detect_and_give_dmg(from_pos, device_unit, user_unit, ra
 		if dmg_ext and dmg_ext.damage_explosion then
 			local ecm_vuln = enemy:base() and enemy:base():char_tweak() and enemy:base():char_tweak().ecm_vulnerability
 
-			if ecm_vuln then
-				local stun = true
+			if ecm_vuln and ecm_vuln ~= 0 then
+				local can_stun = true
 				local brain_ext = enemy:brain()
 
 				if brain_ext then
 					if brain_ext.is_hostage and brain_ext:is_hostage() or brain_ext.surrendered and brain_ext:surrendered() then
-						stun = false
+						can_stun = false
 					end
 				end
 
-				if stun then
-					if enemy:anim_data() and enemy:anim_data().act or ecm_vuln < math_random() then
-						stun = false
+				if can_stun then
+					if enemy:anim_data() and enemy:anim_data().act or enemy:movement():chk_action_forbidden("hurt") or ecm_vuln < math_random() then
+						can_stun = false
 					end
 				end
 
-				if stun then
+				if can_stun then
 					local hit_pos = mvec3_cpy(enemy:movement():m_head_pos())
 					local attack_dir = hit_pos - from_pos
 					mvec3_norm(attack_dir)
@@ -47,8 +41,8 @@ function ECMJammerBase._detect_and_give_dmg(from_pos, device_unit, user_unit, ra
 					local attack_data = {
 						damage = 0,
 						variant = "stun",
-						attacker_unit = alive(user_unit) and user_unit,
-						weapon_unit = alive(device_unit) and user_unit,
+						attacker_unit = attacker,
+						weapon_unit = weapon,
 						col_ray = {
 							position = hit_pos,
 							ray = attack_dir
@@ -60,4 +54,13 @@ function ECMJammerBase._detect_and_give_dmg(from_pos, device_unit, user_unit, ra
 			end
 		end
 	end
+end
+
+local update_original = ECMJammerBase.update
+function ECMJammerBase:update(...)
+	--pain
+	self._unit:m_position(self._position)
+	self._unit:m_rotation(self._rotation)
+
+	update_original(self, ...)
 end
